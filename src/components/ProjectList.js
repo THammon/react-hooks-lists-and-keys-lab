6019442276e028fd1ed0d2309b6b3954d0683e4e
Tristen <tristen.hammon21@gmail.com>
2022-09-20 15:51:07 -0500

    commit tree 5989fa7bb55aa8efe24c23ebefffe471dd7e21b5

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,14 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        <ProjectItem project = {projects[1]}/>
+        <ProjectItem project = {projects[2]}/>
+        <ProjectItem project = {projects[3]}/>
+      </div>
     </div>
   );
 }
